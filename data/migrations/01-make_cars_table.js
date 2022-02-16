ecf5e27f15a9bd26b96c85e6a600bebe75971808
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-15 22:46:48 -0600

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,12 +1,24 @@
+exports.up = function (knex)
+{
+    return knex.schema.createTable('cars', tbl =>
+    {
+        tbl.increments();
 
+        tbl.string('vin', 17).notNullable().unique();
 
+        tbl.string('make', 128).notNullable();
 
+        tbl.string('model', 128).notNullable();
 
+        tbl.integer('mileage', 128).unsigned().notNullable();
 
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+        tbl.string('title', 128);
+
+        tbl.string('transmission', 128);
+    });
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = function (knex)
+{
+    return knex.schema.dropTableIfExists('cars');
 };
